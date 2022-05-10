ae0f6509bb68fde52fd6b3098feb90cb2e9cffd0
Brandon Cha <brandon.cha@redspace.com>
2022-05-10 14:18:41 -0400

    movies app frontend done

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -1,5 +1,14 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {
+  HashRouter as Router, Switch, Route, Link, useParams, useRouteMatch
+} from "react-router-dom";
+
+// Components
+import Home from "./components/Home";
+import Movies from "./components/Movies";
+import Admin from "./components/Admin";
+import Categories from "./components/Categories";
+import OneMovie from "./components/OneMovie";
 
 export default function App() {
   return (
@@ -21,6 +30,9 @@ export default function App() {
                   <Link to="/movies">Movies</Link>
                 </li>
                 <li className="list-group-item">
+                  <Link to="/category">Categories</Link>
+                </li>
+                <li className="list-group-item">
                   <Link to="/admin">Admin</Link>
                 </li>
               </ul>
@@ -29,9 +41,25 @@ export default function App() {
 
           <div className="col-md-10">
             <Switch>
+              <Route path="/movies/:id" component={OneMovie} />
               <Route path="/movies">
                 <Movies />
               </Route>
+
+              <Route exact path="/category">
+                <CategoryPage />
+              </Route>
+              <Route
+                exact
+                path="/category/comedy"
+                render={props => <Categories {...props} title={`Comedy`} />}
+              />
+              <Route
+                exact
+                path="/category/drama"
+                render={props => <Categories {...props} title={`Drama`} />}
+              />
+
               <Route path="/admin">
                 <Admin />
               </Route>
@@ -46,14 +74,26 @@ export default function App() {
   );
 }
 
-function Home() {
-  return <h2>Home</h2>
+function Movie() {
+  let { id } = useParams();
+
+  return <h2>Movie id {id}</h2>
 }
 
-function Movies() {
-  return <h2>Movies</h2>
-}
+function CategoryPage() {
+  let { path, url } = useRouteMatch();
+  return (
+    <div>
+      <h2>Categories</h2>
 
-function Admin() {
-  return <h2>Admin</h2>
+      <ul>
+        <li>
+          <Link to={`${path}/comedy`}>Comedy</Link>
+        </li>
+        <li>
+          <Link to={`${path}/drama`}>Drama</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
